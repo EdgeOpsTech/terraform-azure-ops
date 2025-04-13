e638f8627c09a598cd446aeea3602ab9394ddc54
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-04-13 15:24:13 -0400

    added workflows

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,13 @@ variable "location" {
   type    = string
   default = "eastus"
 }
+
+variable "github_owner" {
+  description = "GitHub organization or owner"
+  type        = string
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+}
