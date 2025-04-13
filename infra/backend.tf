a70fa92b2ccdf4b71a03c495d927393cc07b9bce
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-04-13 14:36:03 -0400

    edited terraform in infra

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,5 +1,11 @@
 terraform {
   backend "azurerm" {
-    # ⇣ <<< paste the backend_config output here >>>
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "edgeopstechtfstate" # ← from bootstrap output
+    container_name       = "tfstate"
+    key                  = "terraform.tfstate"
+    use_oidc             = true
+    client_id            = "fb40e7aa-7931-4675-9295-b0d7620ebf9a" # ← from bootstrap output
   }
 }
+
