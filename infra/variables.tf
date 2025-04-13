bc8b107f7a1e88c9830ae8898047e7ce78361cff
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-04-13 15:25:38 -0400

    edited infra varaibles files

--- infra/variables.tf
+++ infra/variables.tf
@@ -2,13 +2,3 @@ variable "location" {
   type    = string
   default = "eastus"
 }
-
-variable "github_owner" {
-  description = "GitHub organization or owner"
-  type        = string
-}
-
-variable "github_repo" {
-  description = "GitHub repository name"
-  type        = string
-}
