6859f34653fb73bd2bf96735c4bf05ab86fe0516
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-04-17 10:47:56 -0400

    Change backend container name in infra

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-tfstate"
     storage_account_name = "edgeopstechtfstate"
-    container_name       = "tfstate"
+    container_name       = "terraform-azure-ops"
     key                  = "terraform.tfstate"
     use_oidc             = true
     client_id            = "fb40e7aa-7931-4675-9295-b0d7620ebf9a"
