375f1f90da7670d76b0e9043450b4f4a03dbf203
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-07-29 09:26:55 -0400

    added changes in infra and bootstrap

--- infra/main.tf
+++ infra/main.tf
@@ -8,6 +8,7 @@ terraform {
     key                  = "terraform.tfstate"
     use_oidc             = true
     client_id            = "fb40e7aa-7931-4675-9295-b0d7620ebf9a"
+    storage_use_azuread  = true
   }
 
   required_providers {
