49c8ebd6d3485a5a7bf0074a9f900ddd8158480f
Abinay Kumar Devulapally <abinaykumar.d99@gmail.com>
2025-09-30 08:35:11 -0400

    Update location configurations to use 'westus3' across environment files and main.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,7 +1,8 @@
 variable "location" {
   type        = string
   description = "Azure region to deploy resources"
-  default     = "eastus"
+  # default     = "eastus"  # Old region - not allowed by policy
+  default = "westus3" # New allowed region
   validation {
     condition     = length(var.location) > 0
     error_message = "A location must be specified."
